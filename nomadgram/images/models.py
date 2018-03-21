4df9979a804b6180bae08a5c6b3bbc99dc3f5017
whateveruwant <qkrdbals1630@naver.com>
2018-03-21 23:01:50 +0900

    Add LikeModel

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from nomadgram.users import models as user_models
 
 class TimeStampModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -7,9 +8,25 @@ class TimeStampModel(models.Model):
         abstract = True
 
 class Image(TimeStampModel):
+
+    """ Image Model """
+
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True)
 
 class Comment(TimeStampModel):
+
+    """ Comment Model """
+
     message = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.ForeignKey(Image, null=True)
+
+class Like(TimeStampModel):
+
+    """ Like Model """
+
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.ForeignKey(Image, null=True)
