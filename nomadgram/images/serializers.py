020cbf127764c9bc082c296fc8381833087b9df5
whateveruwant <Yumin@ASUS-PYM.localdomain>
2018-03-24 16:37:03 +0900

    Adjust Serializers For User and Commit

--- nomadgram/images/serializers.py
+++ nomadgram/images/serializers.py
@@ -1,12 +1,27 @@
 from rest_framework import serializers
 from . import models
+from nomadgram.users import models as user_models
+
+
+class FeedUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = user_models.User
+        fields = (
+            'username',
+            'profile_image'
+        )
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    creator = FeedUserSerializer()
     class Meta:
         model = models.Comment
-        fields = '__all__'
-
+        fields = (
+            'id',
+            'message',
+            'creator'
+        )
+        
 
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,8 +31,7 @@ class LikeSerializer(serializers.ModelSerializer):
 
 class ImageSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True)
-    likes = LikeSerializer(many=True)
-
+    creator = FeedUserSerializer()
     class Meta:
         model = models.Image
         fields = (
@@ -26,5 +40,6 @@ class ImageSerializer(serializers.ModelSerializer):
             'location',
             'caption',
             'comments',
-            'likes'
+            'like_count',
+            'creator'
         )
--- nomadgram/users/models.py
+++ nomadgram/users/models.py
@@ -17,6 +17,7 @@ class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
+    profile_image = models.ImageField(null=True)
     name = models.CharField(_('Name of User'), blank=True, max_length=255)
     website = models.URLField(null=True) # 공백으로 나둔다.
     bio = models.TextField(null=True)
