c4ba6a3d9a28dad8efd1e5a8cc7b7b2552ec7faf
whateveruwant <Yumin@ASUS-PYM.localdomain>
2018-03-28 21:32:30 +0900

    Setting up JWT

--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
-    url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
+    url(r'^api-token-auth/', obtain_jwt_token),
 
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, admin.site.urls),
