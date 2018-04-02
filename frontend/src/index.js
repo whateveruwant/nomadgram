e96a4e659cc2f5d0dfdb7a84a7fc9fc54753b43d
whateveruwant <Yumin@DESKTOP-BJE9T5P.localdomain>
2018-04-02 23:52:03 +0900

    Redux Middlewares

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './redux/configureStore';
-import './index.css';
-import App from './App';
+import store from 'redux/configureStore';
+import 'index.css';
+import App from 'App';
 
 // console.log(store.getState());
+// store.dispatch({type:"FUCKING"});
 
 ReactDOM.render(
 <Provider store={store}>
