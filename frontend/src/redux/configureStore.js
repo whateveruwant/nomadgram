0422fa589038db83b2292d49296ff1f4e0b9864b
whateveruwant <Yumin@DESKTOP-BJE9T5P.localdomain>
2018-04-03 18:47:18 +0900

    Installing Redux Dev Tools

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { routerReducer, routerMiddleware } from "react-router-redux";
 import createHistory from "history/createBrowserHistory";
+import { composeWithDevTools } from "redux-devtools-extension";
 import users from "redux/modules/users";
 import Reactotron from "ReactotronConfig"
 
@@ -22,7 +23,7 @@ const reducer = combineReducers({
 let store;
 
 if(env === "development") {
-    store = initialState => Reactotron.createStore(reducer, applyMiddleware(...middlewares));
+    store = initialState => Reactotron.createStore(reducer, composeWithDevTools(applyMiddleware(...middlewares)));
 }
 else {
     store = initialState => createStore(reducer, applyMiddleware(...middlewares));
